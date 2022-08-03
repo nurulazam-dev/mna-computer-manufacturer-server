1c6d7a9c72c707c4f237eae7ee26dfb2323e6c0c
mnaofficial <mna.official92@gmail.com>
2022-08-03 20:21:16 +0600

    varify api create

--- index.js
+++ index.js
@@ -99,14 +99,12 @@ async function run() {
       res.send(result);
     });
 
-    // ============================================
     // User add/insetOne API 
     app.post('/users', async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-    // ============================================
 
     //all users API
     app.get('/users', async (req, res) => {
