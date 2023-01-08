790573b3731895a15cdbea071b22cc2aca63b9db
mnaofficial <mna.official92@gmail.com>
2023-01-08 23:36:49 +0600

    products api setup for pagination

--- index.js
+++ index.js
@@ -52,7 +52,18 @@ async function run() {
     ======================================================== */
     //get all products api *
     app.get("/products", async (req, res) => {
-      const products = await productsCollection.find().toArray();
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const query = {};
+      const cursor = productsCollection.find(query);
+      let products;
+      if (page || size) {
+        products = await cursor.skip(page * size).limit(size).toArray();
+      }
+      else {
+        products = await cursor.toArray();
+      }
+      // const products = await productsCollection.find().toArray();
       res.send(products);
     });
 
@@ -276,11 +287,9 @@ async function run() {
     /* ========================================================
                        pagination product count
     ======================================================== */
-    app.get('/productCount', async(req,res)=>{
-      const query ={};
-      const cursor =productsCollection.find(query);
-      const count = await cursor.count();
-      res.send({count})
+    app.get('/productCount', async (req, res) => {
+      const count = await productsCollection.estimatedDocumentCount();
+      res.send({ count })
     })
 
   } finally {
